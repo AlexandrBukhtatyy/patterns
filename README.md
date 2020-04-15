42523fe4b48bfed0e7056b76d298163814593143
Mayak <buhtatyalexander90@gmail.com>
2020-04-15 17:46:25 +0300

    Третья лекция: Prototype

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Репозиторий для практики работы с паттернами
 
 # Ссылки
+* [Паттерны в Javascript реализации](https://designpatternsgame.com/patterns)
 * [refactoring.guru](https://refactoring.guru/ru/design-patterns)
 * [citforum.ru](http://citforum.ru/SE/project/pattern/)
---
+++ lessons/prototype/examples/prototype.ts
@@ -0,0 +1,42 @@
+interface Cloneable<T> {
+    clone: () => T;
+}
+
+class Component implements Cloneable<Component> {
+    public prop1: any;
+    private prop2: any;
+
+    constructor(initProms: {prop2: any}) {
+        this.prop2 = initProms.prop2;
+    }
+
+    public clone(): Component {
+        const initParams: {prop2: any} = {
+            prop2: this.prop2
+        };
+        const clone = new Component(initParams);    // Прокидивыем параметры инициализации через конструктор для Private свойств
+
+        clone.prop1 = this.prop1;   // Можем менять Public свойтсва
+
+        return clone;
+    }
+
+}
+
+
+const initParams: any = {
+    prop2: 2,
+};
+const component: Component = new Component(initParams);
+
+const componentLink: Component = component;
+console.log(component === componentLink);   // true
+
+const componentClone: Component = component.clone();
+console.log(component === componentClone);  // false
+
+
+/* BAD CODE */
+const componentBCExample: Component = new Component(initParams);
+const componentCloneBCExample1: Component = {...componentBCExample};
+const componentCloneBCExample2: Component = Object.assign({}, componentBCExample);
